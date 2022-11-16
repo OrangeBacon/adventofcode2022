59d7462d600e7b87e7c97c9cc5d1df15f64ae138
OrangeBacon <computer.backup.15@gmail.com>
2022-11-16 17:38:18 +0000

    Start creating template

---
+++ src/day01.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+use libaoc::{aoc, AocResult, Timer};
+
+#[aoc("", "")]
+pub fn solve(timer: &mut Timer, input: &str) -> Result<AocResult> {
+    timer.lap("Parse");
+
+    timer.lap("Part 1");
+
+    timer.lap("Part 2");
+
+    Ok(AocResult::new("", ""))
+}
