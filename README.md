7eb82ac97737c7013ebed02fbcb04a1d4fbfbe8a
OrangeBacon <computer.backup.15@gmail.com>
2022-12-02 13:25:13 +0000

    Neater code for day 2

---
+++ README.md
@@ -0,0 +1,19 @@
+# Advent of Code 2022
+
+My solutions to Advent of Code 2022, written in rust.
+The solutions are in the files `src/dayXX.rs`, the first commit to those files
+is what I used to solve the problem, any further commits should be improving the
+code quality, speed, etc. after I have solved it.
+
+## Usage
+
+Usage: `adventofcode2022 [DAYS]...`
+
+Example: `adventofcode2020 1 2 3` to run days 1, 2 and 3
+
+Arguments:
+- `[DAYS]...` The days to run [default: The most recent day solved].
+
+Options:
+- `-h`, `--help` Print help information
+- `-V`, `--version` Print version information
--- src/day02.rs
+++ src/day02.rs
@@ -1,68 +1,69 @@
 use itertools::Itertools;
 use libaoc::Timer;
 
+#[derive(Debug, Clone, Copy)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+use Move::*;
+
 pub fn solve(timer: &mut Timer, input: &str) -> () {
     let data = std::fs::read_to_string("./data/day02.txt").unwrap();
     let data = data
         .lines()
-        .map(|l| l.split_whitespace().collect_vec())
+        .map(|l| l.split_whitespace().map(Move::from_str).collect_vec())
         .collect_vec();
 
     timer.lap("Parse");
 
     let mut score = 0;
     for line in &data {
-        score += match line[1] {
-            "X" => 1,
-            "Y" => 2,
-            "Z" => 3,
-            _ => panic!("Invalid data"),
-        };
-        score += match (line[0], line[1]) {
-            ("A", "Y") | ("B", "Z") | ("C", "X") => 6,
-            ("A", "X") | ("B", "Y") | ("C", "Z") => 3,
-            _ => 0,
-        };
+        score += line[1] as i32;
+        score += line[0].win_score(line[1]);
     }
     timer.lap("Part 1");
 
     let mut score2 = 0;
     for line in &data {
-        let my_move = match line[1] {
-            "Y" => match line[0] {
-                "A" => "X",
-                "B" => "Y",
-                "C" => "Z",
-                _ => panic!(),
-            },
-            "X" => match line[0] {
-                "A" => "Z",
-                "B" => "X",
-                "C" => "Y",
-                _ => panic!(),
-            },
-            "Z" => match line[0] {
-                "A" => "Y",
-                "B" => "Z",
-                "C" => "X",
-                _ => panic!(),
-            },
-            _ => panic!(),
-        };
-        score2 += match my_move {
-            "X" => 1,
-            "Y" => 2,
-            "Z" => 3,
-            _ => panic!("Invalid data"),
-        };
-        score2 += match (line[0], my_move) {
-            ("A", "Y") | ("B", "Z") | ("C", "X") => 6,
-            ("A", "X") | ("B", "Y") | ("C", "Z") => 3,
-            _ => 0,
-        };
+        let my_move = line[1].to_should_win(line[0]);
+        score2 += my_move as i32;
+        score2 += line[0].win_score(my_move);
     }
     timer.lap("Part 2");
 
     println!("Part 1: {score}");
     println!("Part 2: {score2}");
+}
+
+impl Move {
+    fn from_str(s: &str) -> Self {
+        match s {
+            "A" | "X" => Rock,
+            "B" | "Y" => Paper,
+            "C" | "Z" => Scissors,
+            _ => panic!("Invalid data"),
+        }
+    }
+
+    fn to_should_win(&self, other: Self) -> Self {
+        match (self, other) {
+            (Paper, _) => other,
+            (Rock, Rock) => Scissors,
+            (Rock, Paper) => Rock,
+            (Rock, Scissors) => Paper,
+            (Scissors, Rock) => Paper,
+            (Scissors, Paper) => Scissors,
+            (Scissors, Scissors) => Rock,
+        }
+    }
+
+    fn win_score(&self, other: Self) -> i32 {
+        match (self, other) {
+            (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => 6,
+            (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => 3,
+            _ => 0,
+        }
+    }
 }
