bcc012270140081e375651fbeb96e888c0097b07
OrangeBacon <computer.backup.15@gmail.com>
2022-12-09 07:46:59 +0000

    day 9 clean up

--- src/day09.rs
+++ src/day09.rs
@@ -4,35 +4,26 @@ use itertools::Itertools;
 use libaoc::Timer;
 
 pub fn solve(timer: &mut Timer, input: &str) -> () {
-    let data = input
+    let commands = input
         .lines()
         .map(|line| line.split_once(' ').unwrap())
         .map(|(dir, dist)| (dir, dist.parse::<i32>().unwrap()))
         .collect_vec();
     timer.lap("Parse");
 
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
-    let mut visited = HashSet::from([tail]);
-    for command in &data {
-        for _ in 0..command.1 {
-            match command.0 {
-                "R" => head.0 += 1,
-                "U" => head.1 += 1,
-                "L" => head.0 -= 1,
-                "D" => head.1 -= 1,
-                _ => panic!(),
-            }
-            tail = move_tail(head, tail);
-            visited.insert(tail);
-        }
-    }
-    let part_1 = visited.len();
+    let part_1 = move_rope(&commands, &mut [(0, 0); 2]);
     timer.lap("Part 1");
 
-    let mut rope = vec![(0, 0); 10];
+    let part_2 = move_rope(&commands, &mut [(0, 0); 100]);
+    timer.lap("Part 2");
+
+    println!("Part 1: {part_1}");
+    println!("Part 2: {part_2}");
+}
+
+fn move_rope(commands: &[(&str, i32)], rope: &mut [(i32, i32)]) -> usize {
     let mut visited = HashSet::from([*rope.last().unwrap()]);
-    for command in data {
+    for command in commands {
         for _ in 0..command.1 {
             match command.0 {
                 "R" => rope[0].0 += 1,
@@ -41,19 +32,15 @@ pub fn solve(timer: &mut Timer, input: &str) -> () {
                 "D" => rope[0].1 -= 1,
                 _ => panic!(),
             }
-            move_rope(&mut rope);
+            move_knots(rope);
             visited.insert(*rope.last().unwrap());
         }
     }
 
-    let part_2 = visited.len();
-    timer.lap("Part 2");
-
-    println!("Part 1: {part_1}");
-    println!("Part 2: {part_2}");
+    visited.len()
 }
 
-fn move_rope(rope: &mut [(i32, i32)]) -> (i32, i32) {
+fn move_knots(rope: &mut [(i32, i32)]) -> (i32, i32) {
     let mut tail = (0, 0);
     for idx in 0..rope.len() - 1 {
         tail = move_tail(rope[idx], rope[idx + 1]);
